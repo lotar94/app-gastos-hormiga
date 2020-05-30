9c97e80ed516132c1722a026a64aa807a5ebdd6b
lotar94 <lotar.94@gmail.com>
2020-05-30 18:02:48 -0400

    Se genera primer componente

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+       <LinearGradient
+      //  99D5C2
+      //  77AAA5
+      //  417E7E
+          colors={['#99D5C2', '#357676', 'transparent']}
+          style={{
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            top: 0,
+            height: 900,
+          }}
+        />
     </View>
   );
 }
@@ -12,7 +26,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#357676',
     alignItems: 'center',
     justifyContent: 'center',
   },
